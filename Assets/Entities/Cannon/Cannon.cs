48aaf1d3ea7387545e1dc89658c3b03e116cf4f9
JoshuaTaylorBw <JoshuaTaylorBW@gmail.com>
2017-07-20 00:10:25 -0600

    Add Cannon Basics and Spot Writer and New Hard Level Add writers and grass to Hard.
    Change left pushes to not trigger incorrectly (hopefully)
    
    Fix broken Left Push on Protagonist.cs

---
+++ Assets/Entities/Cannon/Cannon.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+	private CharacterController controller;
+	public float CannonAngle;
+	public float CannonFrequency;
+	public float CannonSpeed;
+	public bool CannonIsOnScreen = true;
+	private GameObject cannonBall;
+
+	// Use this for initialization
+	void Start () {
+		CannonAngle = transform.rotation.eulerAngles.z; 
+		StartCoroutine(spawnCannonBalls());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void MakeCannonball(){
+		cannonBall = Instantiate(Resources.Load("Cannon-Ball"), transform.position, Quaternion.identity) as GameObject;
+		cannonBall.SendMessage("SetAngle", CannonAngle);	
+		cannonBall.SendMessage("SetSpeed", CannonSpeed);
+	}
+
+	IEnumerator spawnCannonBalls()
+	{
+		while (true)
+		{
+			while(CannonIsOnScreen){
+				MakeCannonball();
+				yield return new WaitForSeconds(CannonFrequency);
+			}
+		}
+	}
+}
---
+++ Assets/Entities/Cannon/CannonBall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBall : MonoBehaviour {
+
+	private CharacterController controller;
+	public float ballAngle;
+	public float ballSpeed;
+	public Vector3 whateverThisIs;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void SetAngle(float angle){
+		ballAngle = angle;
+		transform.localEulerAngles = new Vector3(0, 0, angle);
+	}
+
+	void SetSpeed(float speed){
+		ballSpeed = speed;	
+	}
+
+	void Update () {
+		//whateverThisIs = transform.TransformDirection(Vector3.forward) * ballSpeed;
+		controller.Move(transform.right * Time.deltaTime * ballSpeed);
+	}
+
+
+}
